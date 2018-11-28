54387d47df4d1810f5e9adaaa38f28547d6d567e
Kanhaiya Thakur <oceank007@gmail.com>
2018-11-29 02:01:30 +0530

    Adding changes

--- src/App.js
+++ src/App.js
@@ -13,19 +13,61 @@ class App extends Component {
 
   // Add "Handler" at end to indicate that it's used by event handlers
   // You can define any name though
-  switchNameHandler = () => {
-    console.log("Clicked");
+  switchNameHandler = (newName) => {
+    // console.log("Clicked");
+    
+    // We can't change state directly like this
+    // this.state.persons[0].name = 'Life'; // DON'T DO THIS
+
+    // setState just merges the differences, it doesn't replace whole state
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Sandeep', age: 28 },
+        { name: 'Ashutosh', age: 28 }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Ocean', age: 28 },
+        { name: event.target.value, age: 28 },
+        { name: 'Ashutosh', age: 27 }
+      ]
+    });
   }
 
   render() {
+    // It's hard to use hovering styles while using inline styling
+    const style = {
+      // javascript properties
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Gaming</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        {/* It must not be used, since it creates performance issues, use .bind() instead */}
+        <button style={style} onClick={() => this.switchNameHandler('Life')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        // Best way is to use .bind()
+        clickit={this.switchNameHandler.bind(this, 'Ocean!!!')}
+        changed={this.nameChangedHandler}>My Hobbies: Gaming</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
+ // Due to webpack, we can import css into js file
+ // But it won't be bundled together, .css will be bundled separately
+import './Person.css'; 
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old.</p>
+        <div className="Person">
+            <p onClick={props.clickit}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     );
 };
