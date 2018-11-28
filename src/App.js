a0feb76750b7176484b0602070584daf32247831
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-11-28 16:50:50 +0530

    Adding changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'; // Can give any other name as well
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Ocean', age: 28 },
+      { name: 'Sandeep', age: 28 },
+      { name: 'Ashutosh', age: 27 }
+    ]
+  }
+
+  // Add "Handler" at end to indicate that it's used by event handlers
+  // You can define any name though
+  switchNameHandler = () => {
+    console.log("Clicked");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Gaming</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
