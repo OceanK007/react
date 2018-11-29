fe11b3917cb1f794a6b857fe108363c24394b705
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-11-29 17:00:20 +0530

    Adding css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'; // Can give any other name as well
 
 class App extends Component {
@@ -36,19 +36,8 @@ class App extends Component {
   }
 
   render() {
-    // It's hard to use hovering styles while using inline styling
-    // We can use Radium to apply hovering styles
-    const style = {
-      // javascript properties
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
     
     if(this.state.showPersons) {
       persons = (
@@ -64,24 +53,24 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
-    const classes = [];    
+    const assignedClasses = [];    
     if(this.state.persons.length <= 2) {
-      classes.push('red');  // classes = ['red'];
+      assignedClasses.push(classes.red);  // classes = ['red'];
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     // .join(' ') : to convert array to string with space between elements 'red bold'
     return ( 
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         {/* It must not be used, since it creates performance issues, use .bind() instead */}
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}        
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,12 @@
 import React from 'react';
  // Due to webpack, we can import css into js file
  // But it won't be bundled together, .css will be bundled separately
-import './Person.css'; 
+import classes from './Person.css'; 
 
 const person = (props) => {
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
