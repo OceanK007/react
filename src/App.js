be9f7c414610963bd8416457ad8c4405c59e0260
Kanhaiya Thakur <oceank007@gmail.com>
2018-12-08 23:26:51 +0530

    Adding changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component, Suspense } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-import Blog from './containers/Blog/Blog';
+// import Posts from './containers/Posts';  // WIll be loaded lazily
+import User from './containers/User';
+import Welcome from './containers/Welcome';
+
+// Only use default imports, named imports are not supported by import() method.
+// here return import() means, whatever will come in this import() parenthesis will
+// only imported when this function executed.
+const Posts = React.lazy(() => import('./containers/Posts'));  
 
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="my-app">
       <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
+        {/* React-Fragment (added form v16.6) is same like Auxiliary Component, which 
+        is used to wrap component. It doesn't render a real dom element. */}
+        <React.Fragment>
+          <nav>
+            <NavLink to="/user">User Page</NavLink> |&nbsp;
+            <NavLink to="/posts">Posts Page</NavLink>
+          </nav>
+          <Route path="/" component={Welcome} exact />
+          <Route path="/user" component={User} />
+          {/* <Route path="/posts" component={Posts} /> */}
+          <Route path="/posts" render={() => (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Posts />
+            </Suspense>
+          )} />
+        </React.Fragment>
       </BrowserRouter>
     );
   }
